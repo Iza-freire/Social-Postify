24ee7e362334a99bb682902fba829315fa168da4
IzaFreire <kariny.frei.2000@gmail.com>
2023-07-16 20:02:55 -0300

    feat: module user

---
+++ src/user/user.repository.ts
@@ -0,0 +1,40 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { CreateUserDto } from "./dto/create-ser.dto";
+
+@Injectable()
+export class UserRepository {
+
+    constructor(private readonly prisma: PrismaService) { }
+
+
+    async create(userDTO: CreateUserDto) {
+        return this.prisma.user.create({
+            data: userDTO
+        })
+    }
+
+    async get(id: number) {
+        return this.prisma.user.findFirst({
+            where: { id }
+        })
+    }
+    async getByEmail(email: string) {
+        return this.prisma.user.findMany({
+            where: { email }
+        })
+    }
+
+    async delete(id: number) {
+        return this.prisma.user.delete({
+            where: { id },
+        });
+    }
+
+    async update(id: number, userDTO: CreateUserDto) {
+        return this.prisma.user.update({
+            where: { id },
+            data: userDTO,
+        });
+    }
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,45 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UserRepository } from './user.repository';
+import { CreateUserDto } from './dto/create-ser.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UserService {
+
+    constructor(private readonly userRepository: UserRepository) { }
+
+    async create(userDTO: CreateUserDto) {
+        const hashedPassword = await bcrypt.hash(userDTO.password, 10);
+        const user = await this.userRepository.create({
+            ...userDTO,
+            password: hashedPassword
+        });
+        return user;
+    }
+
+    async get(id: number) {
+        const user = await this.userRepository.get(id);
+        if (!user) throw new NotFoundException();
+        return user;
+    }
+
+    async getByEmail(email: string) {
+        const user = await this.userRepository.getByEmail(email);
+        return user;
+    }
+
+    async delete(id: number) {
+        const user = await this.userRepository.get(id);
+        if (!user) throw new NotFoundException();
+
+        return this.userRepository.delete(id);
+    }
+
+    async update(id: number, userDTO: CreateUserDto) {
+        const user = await this.userRepository.get(id);
+        if (!user) throw new NotFoundException();
+
+        const updatedUser = { ...user, ...userDTO };
+        return this.userRepository.update(id, updatedUser);
+    }
+}
