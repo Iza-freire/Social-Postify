86be734eb3d388525e6f51578829ed4f0c26a312
IzaFreire <kariny.frei.2000@gmail.com>
2023-07-16 18:22:30 -0300

    feat: create prisma migrate

---
+++ prisma/migrations/20230716211933_users_and_publications/migration.sql
@@ -0,0 +1,31 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "avatar" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "publications" (
+    "id" SERIAL NOT NULL,
+    "image" VARCHAR NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "text" TEXT NOT NULL,
+    "dateToPublish" TIMESTAMP(3) NOT NULL,
+    "published" BOOLEAN NOT NULL,
+    "socialMedia" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "publications_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "publications" ADD CONSTRAINT "publications_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
